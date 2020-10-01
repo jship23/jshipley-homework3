390af84290aca1d6ec52117ac51f71846ce852d9
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-09-30 18:21:33 -0600

    some changes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase =letter.toUpperCase();
+var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+var symbolArr = symbol.split("");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
