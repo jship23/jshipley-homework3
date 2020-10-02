f0b7c5ffac7e26864a1d64e1397a1b7d7d6e55fd
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-01 22:11:09 -0600

    some minor changes

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var characters = {
+  "upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "lower": "abcdefghijklmnopqrstuvwxyz",
+  "numbers": "0123456789",
+  "special": "!@#$%^&*(){}[]=<>/,.|~?",
+};
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase =letter.toUpperCase();
-var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-var symbolArr = symbol.split("");
+
+var generateBtn = document.querySelector("#generate");
+  charLength = prompt("How many characters do you want your password to have?")
+
+
+
+  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ 
