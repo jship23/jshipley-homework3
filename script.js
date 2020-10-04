c8b208f9deba758733cfec88801c6c06555cdbbb
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-03 20:03:33 -0600

    some more changes

--- script.js
+++ script.js
@@ -1,53 +1,82 @@
 // Assignment Code
-var characters = {
-  "upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "lower": "abcdefghijklmnopqrstuvwxyz",
-  "numbers": [0,1,2,3,4,5,6,7,8,9],
-  "special": "!@#$%^&*(){}[]=<>/,.|~?",
-};
+var characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]=<>/,.|~?";
+//empty string
+var passwordContains = "";
 
 //click on generate password button
 var generateBtn = document.querySelector("#generate")
-generateBtn.addEventListener('click', passwordCriteria);
+generateBtn.addEventListener("click", writePassword);
 
 //prompt user for password criteria
-function passwordCriteria() {
-
+function generatePassword() {
+  
+  
   //prompt how many characters
   var charLength = parseInt(prompt("How many characters do you want your password to have?"));
+  if (charLength < 8) {
+   alert("Password must be more than 8 characters")
+   return;
+  }
+  if(charLength > 128) {
+   alert("Password must be less than 128 characters")
+   return;
+  }
   console.log(charLength)
-  //set the password lenght = to character lenghth input
-  var passwordLength = charLength
-  console.log(passwordLength)
 
   //include uppercase
   var includeUpp = confirm("Do you want to include capital letters?")
   console.log(includeUpp)
+
+
+
   //include lowercase
   var includeLow = confirm("Do you want to include lowercase letters?")
   console.log(includeLow)
+
+  if(includeLow) {
+    passwordContains += characters.lower;
+  };
+
   //include numbers
   var includeNum = confirm("Do you want to include numbers?")
   console.log(includeNum)
+
+  if(includeNum) {
+    passwordContains += characters.number;
+  };
+
+
   //include special characters
   var includeSym = confirm("Do you want to include special characters?")
   console.log(includeSym)
 
-};
+  if(includeSym) {
+    passwordContains += characters.symbol;
+  };
 
-  
+  if (includeUpp === false && includeLow === false && includeNum === false && includeSym === false){
+  alert("Your password must contain at least one uppercase letter, lowercase letter, number, or special character")
+  };
+
+  var password = "";
+  for(var i=characters; i<0; i++) {
+  password += passwordContains[Math.floor(Math.random() * passwordContains.length)]
+  var password = generatePassword();
+  };
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
+  
+  console.log(password);
 
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+};
 
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
  
